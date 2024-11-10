1cb1d0e1dd4127e5146e705ce4a9daee0b92f414
Jaytonic <hello@jaytonic.io>
2024-11-10 11:19:01 +0100

    Adding missing GatheringSkill and a new unit test

---
+++ tests/ArtifactsMMO.NET.Integration.Tests/Endpoints/Resources/ResourcesEndpointTests.cs
@@ -0,0 +1,41 @@
+﻿using ArtifactsMMO.NET.Objects;
+using ArtifactsMMO.NET.Objects.Resources;
+using ArtifactsMMO.NET.Queries;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ArtifactsMMO.NET.Integration.Tests.Endpoints.Resources
+{
+    public class ResourcesEndpointTests : IClassFixture<TestFixture>
+    {
+        private readonly IArtifactsMMOClient _client;
+        private readonly IConfiguration _configuration;
+
+        public ResourcesEndpointTests(TestFixture fixture)
+        {
+            _client = fixture.ServiceProvider.GetRequiredService<IArtifactsMMOClient>();
+            _configuration = fixture.Configuration;
+        }
+
+
+        [Fact]
+        public async Task DefaultResourcesQuery_ShouldReturnExpectedData()
+        {
+            //Arrange
+            int page = 1;
+            int pageSize = 100;
+
+            //Act
+            PagedResponse<Resource>? result = await _client.Resources.GetAsync(new ResourcesQuery(page: page, size: pageSize));
+
+
+            //Assert
+
+            Assert.NotNull(result);
+            Assert.True(result.Total > 0);
+            Assert.NotNull(result.Data);
+            var firstResource = result.Data.FirstOrDefault();
+            Assert.NotNull(firstResource);
+        }
+    }
+}
